5f3570d8b6de0401750d3f9eca1389494d02ea2f
dw-liedji <liedjiwenkack@gmail.com>
2022-03-01 19:31:10 +0100

    Working with array immutability added.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const person = {
-  name: "liedji",
-  address: { country: "Cameroon", city: "Dschang" },
-};
-const updated_person = {
-  ...person,
-  address: { ...person.address, city: "Bafoussam" },
-  name: "liedji updated version 2",
-};
-console.log(person);
-console.log(updated_person);
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// adding
+const index = numbers.indexOf(2);
+const added = [...numbers.slice(0, index), 25, ...numbers.slice(index)];
+console.log(added);
+
+// removing
+const removed = numbers.filter((number) => number !== 1);
+console.log(removed);
+
+// updating
+const updated = numbers.map((number) =>
+  number === 5 ? number ** 2 : number / 2
+);
+console.log(updated);
