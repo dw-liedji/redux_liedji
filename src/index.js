4da3347da7f24c3473207d2421c4ac907e0815f6
dw-liedji <liedjiwenkack@gmail.com>
2022-03-01 22:57:41 +0100

    Add managing objects mutation as immutable using Immer.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+import { produce } from "immer";
 
-// adding
-const index = numbers.indexOf(2);
-const added = [...numbers.slice(0, index), 25, ...numbers.slice(index)];
-console.log(added);
+let book = { name: "The Redux bible book by Liedji.", isPublished: false };
+let updated = produce(book, (draftBook) => {
+  draftBook.isPublished = true;
+});
 
-// removing
-const removed = numbers.filter((number) => number !== 1);
-console.log(removed);
-
-// updating
-const updated = numbers.map((number) =>
-  number === 5 ? number ** 2 : number / 2
-);
-console.log(updated);
+console.log(book, updated);
