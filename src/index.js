b1397c8115140864b5b841d69bc6057d030556be
dw-liedji <liedjiwenkack@gmail.com>
2022-03-01 19:08:42 +0100

    Working with object immutability added.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const person = {
+  name: "liedji",
+  address: { country: "Cameroon", city: "Dschang" },
+};
+const updated_person = {
+  ...person,
+  address: { ...person.address, city: "Bafoussam" },
+  name: "liedji updated version 2",
+};
+console.log(person);
+console.log(updated_person);
