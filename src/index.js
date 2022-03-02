fc22ea93cd38f8582fa6e0bc05575c90921378bd
dw-liedji <liedjiwenkack@gmail.com>
2022-03-02 20:18:58 +0100

    Code refactoring.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import store from "./store/store";
+import store from "./store/configureStore";
 import * as actions from "./store/bugs";
 
 const unsubscribe = store.subscribe(() => {
@@ -13,8 +13,11 @@ store.dispatch(actions.bugAdded({ description: "Un simple bug 4" }));
 store.dispatch(actions.bugResolved({ id: 1 }));
 store.dispatch(actions.bugResolved({ id: 2 }));
 store.dispatch(actions.bugResolved({ id: 3 }));
-
-unsubscribe();
+store.dispatch(actions.bugResolved({ id: 4 }));
 
 store.dispatch(actions.bugRemoved({ id: 1 }));
+store.dispatch(actions.bugRemoved({ id: 2 }));
+store.dispatch(actions.bugRemoved({ id: 3 }));
+store.dispatch(actions.bugRemoved({ id: 4 }));
+unsubscribe();
 console.log(store.getState());
